3db9b573d50d5faa2c49445d57aa58131044032b
Notes <deepa.sri2912@gmail.com>
2024-01-20 19:59:08 +0530

    post css

--- src/Components/NoteList.jsx
+++ src/Components/NoteList.jsx
@@ -5,7 +5,7 @@ import style from "./NoteList.module.css"
 const NoteList = () => {
   return (
    
-   <ul id={style.list}>
+   <ul className={style.list}>
     <Note name="Meena" like="loves chocolates"/>
     <Note name="Deepa" like="loves chocolates"/>
    </ul>
