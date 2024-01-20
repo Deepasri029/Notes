1e18a166bffc4623c8f3ab678909c12ae6fdbc21
Notes <deepa.sri2912@gmail.com>
2024-01-20 21:10:32 +0530

    newnote

---
+++ src/Components/NewNote.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NewNote = () => {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">Note</label>
+                <textarea name="" id="" cols="30" rows="5"></textarea>
+                <label htmlFor="">date</label>
+                <input type="date" />
+            </form>
+        </div>
+    )
+}
+
+export default NewNote
